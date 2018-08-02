82197da86b282ccbdb88b35ba09fe0cff41b121f
ashleywang1 <ashwang168@gmail.com>
2018-08-01 21:08:12 -0400

    Use library/nginx instead of tutum/nginx

--- web/server/start.sh
+++ web/server/start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# Import Gdrive creds
-python3 manage.py update_gdrive_creds -f keys.json
-
 # First time tmdb startup
 python3 manage.py makemigrations tmdb && python3 manage.py migrate
+
+# Import Gdrive creds
+python3 manage.py update_gdrive_creds -f keys.json
 
 # Start the server
 /usr/bin/gunicorn ectc_tm_server.wsgi:application -w 2 -b :8000
