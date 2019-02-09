a78c7e0052693f9f8da2b0b227e47f7bbfd3b7d4
ashleywang1 <ashwang168@gmail.com>
2019-02-09 17:44:59 -0500

    Load the keys in manually, not with a keys.json file

--- web/start.sh
+++ web/start.sh
@@ -5,7 +5,7 @@ cd server
 python3 manage.py makemigrations tmdb && python3 manage.py migrate
 
 # Import Gdrive creds
-python3 manage.py update_gdrive_creds -f keys.json
+# python3 manage.py update_gdrive_creds -f keys.json
 
 # Start the server
 # /usr/bin/gunicorn ectc_tm_server.wsgi:application -w 2 -b :8000
